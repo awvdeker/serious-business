2875f8428945e9d54342c1945e9870f318fdb9bb
awvdeker <alexander.vandekerckhove@gmail.com>
2019-04-02 15:09:28 +0200

    change classnames

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//checking the day
+//checking the day: number 0-6 : 0=sunday
 
 var d = new Date();
 
@@ -8,3 +8,7 @@ document.getElementById("day").innerHTML = d.getDay();
 //checking the time
 
 document.getElementById("time").innerHTML = d.getHours()+":"+d.getMinutes();
+
+
+//Compare it to the business hours
+//Declare if the business is open
