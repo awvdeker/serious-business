3f1b8574588056a586b7777645be6ab2eeef6eeb
awvdeker <alexander.vandekerckhove@gmail.com>
2019-04-02 17:16:13 +0200

    declaring business open/closed: works!

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 //checking the day: number 0-6 : 0=sunday
 
 var d = new Date();
+var dDay = d.getDay();
+var dHours = d.getHours();
+var dMinutes = d.getMinutes();
 
-document.getElementById("day").innerHTML = d.getDay();
 
+document.getElementById("day").innerHTML = dDay;
 
 //checking the time
 
-document.getElementById("time").innerHTML = d.getHours()+":"+d.getMinutes();
+document.getElementById("time").innerHTML = dHours+":"+dMinutes;
 
 
 //Compare it to the business hours
 //Declare if the business is open
+
+
+
+var x = document.getElementsByClassName(dDay);
+var later = false;
+var earlier = false;
+
+if (dHours<x[0].innerHTML){ later = false;}
+else if (dHours>x[0].innerHTML){ later = true;}
+else if ((dHours==x[0].innerHTML) && (dMinutes>=x[1].innerHTML)){later = true;}
+else {later = false;}
+
+
+if (dHours>x[2].innerHTML){ earlier = false;}
+else if (dHours<x[2].innerHTML){ earlier = true;}
+else if ((dHours==x[2].innerHTML) && (dMinutes<=x[3].innerHTML)){earlier = true;}
+else {earlier = false;}
+
+
+if ((later==true) && (earlier==true)){
+  document.getElementById("openOrClosed").innerHTML="open";
+} else {
+  document.getElementById("openOrClosed").innerHTML="closed";
+}
